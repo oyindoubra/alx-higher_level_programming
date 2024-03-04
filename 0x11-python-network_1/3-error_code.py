e45b67163c3886458e56acc14fea78998d3bfe07
oyindoubra <doubbynath@gmail.com>
2024-03-04 11:59:39 -0800

    py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script tat takes in url, send a request to url"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
