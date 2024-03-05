7e831917918e8202a3c21ef1a3aae1f1a7f731a9
oyindoubra <77353574+oyindoubra@users.noreply.github.com>
2024-03-05 20:13:53 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#take in a URL, send a request to that URL,and display the size of the body of the response
+#take in a URL send a request to that URLand display the size of the body of the response
 curl -s "$1" | wc -c
-
