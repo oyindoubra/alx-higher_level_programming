f1710e58f6c7938837d53daf5bf11cee683757e5
oyindoubra <77353574+oyindoubra@users.noreply.github.com>
2024-03-05 20:34:40 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,10 +2,8 @@
 """fetches status page"""
 import requests
 
-
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
-
